eddba20bd1d5421008a8b3f5ab98779b26ad8f71
nithyaa-pt6485 <nithyaa-pt6485@NITHYAA-PT6485>
2022-11-07 13:28:45 +0530

    Add project files.

---
+++ Employee.cs
@@ -0,0 +1,73 @@
+﻿using System.Reflection.Emit;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace ConsoleApp4
+{
+    [Serializable]
+    public class Employee
+    {
+        public string Name;
+
+        public string Phone;
+
+        public string DoB;
+
+        public string Department;
+
+        public int Salary;
+
+        public static void Serializer()
+        {
+            
+            Employee emp = new Employee
+            {
+                
+                Name = "Aaron",
+                Phone = "90012365",
+                DoB = "24.09.1982",
+                Department = "DevOps",
+                Salary = 32000,
+            
+            };
+
+            BinaryFormatter bf = new BinaryFormatter();     //serializing data to binary
+
+            FileStream fsout = new FileStream("employee.binary", FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+            
+            using (fsout)  
+            {  
+                bf.Serialize(fsout, emp);  
+                Console.WriteLine("Object Serialized");  
+            }
+            
+        }
+
+        public static void Deserializer()
+        {
+            Employee emp = new Employee();
+            BinaryFormatter bf = new BinaryFormatter();     //desrialixing from binary 
+
+            FileStream fsin = new FileStream("employee.binary", FileMode.Open, FileAccess.Read, FileShare.None);
+
+            using (fsin)
+            {
+                emp = (Employee)bf.Deserialize(fsin);
+                Console.WriteLine("Object Deserialised");
+                emp.Display();
+            }
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Name : {this.Name}");
+            Console.WriteLine($"Phone : {this.Phone}");
+            Console.WriteLine($"DoB : {this.DoB}");
+            Console.WriteLine($"Department : {this.Department}");
+            Console.WriteLine($"Salary: {this.Salary}");
+        }
+
+    }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            string FilePath = @"E:\MyFile.txt";
+            FileStream fileStream = new FileStream(FilePath, FileMode.OpenOrCreate);
+            fileStream.Close();
+
+            fileStream = new FileStream(FilePath, FileMode.Append);                 //output stream
+            byte[] bdata = Encoding.Default.GetBytes("C# Is an Object Oriented Programming Language \n");
+            fileStream.Write(bdata, 0, bdata.Length);
+            fileStream.Close();
+
+
+
+            //StreamWriter and StreamReader
+
+            fileStream = new FileStream(FilePath, FileMode.Open, FileAccess.Read);      //input stream
+
+            string data;
+            using (StreamReader streamReader = new StreamReader(fileStream))
+            {
+                data = streamReader.ReadToEnd();
+            }
+            Console.WriteLine(data);
+            Console.ReadLine();
+
+
+
+            StreamWriter sw = new StreamWriter(FilePath, true);
+
+            Console.WriteLine("Enter the Text that you want to write on File");
+            string str = Console.ReadLine();
+
+
+            sw.WriteLine(str);          // To write the data into the stream
+
+            sw.Flush();             // Clears all the buffers
+
+            sw.Close();             // To close the stream        
+
+            int a, b, result;
+            a = 15;
+            b = 20;
+            result = a + b;
+
+            using (StreamWriter writer = new StreamWriter(FilePath, true))
+            {
+                writer.WriteLine("Sum of {0} + {1} = {2}", a, b, result);
+            }
+
+            //Binary Files;
+            Console.Clear();
+            Console.WriteLine("Writing to and operating on a binary file \n ");
+            BinaryWriterDemo.BinaryDemo();
+
+            //Serializing and Deserializing
+            Console.Clear();
+            Console.WriteLine("Serializing - convert object into a form that can be written into a stream.");
+            Console.WriteLine("Deserializing - get back the serialized object and store it in memory. \n "); 
+            Employee.Serializer();
+            Employee.Deserializer();
+        }
+    }
+}
